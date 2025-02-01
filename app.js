2c60fa54224b02056cf535cecd0221ce24d44f8d
mazumdaratik <rjatik990@gmail.com>
2025-02-01 23:44:16 +0600

    Real time chat applications project scaffold

---
+++ app.js
@@ -0,0 +1,46 @@
+//external imports
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+//internal imports
+const {notFoundHandler, errorHandler} = require('./middlewares/common/errorHandler');
+
+const app = express();
+dotenv.config();
+
+//database connection
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then( () => console.log("database connected"))
+.catch( err => console.log(err));
+
+//request-parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+
+//404 not found handler
+app.use(notFoundHandler);
+
+//common error handling
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listening on port ${process.env.PORT}`);
+})
