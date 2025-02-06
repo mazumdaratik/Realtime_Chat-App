37d3e5d3e142d4df8e11aaf70a0d66f5ea1f7f98
mazumdaratik <rjatik990@gmail.com>
2025-02-06 23:08:37 +0600

    login router added

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,11 +1,14 @@
 //external imports 
 const express = require('express');
-const {getLogin} = require('../controller/loginController');
+const {getLogin, login} = require('../controller/loginController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse')
 
 const router = express.Router();
 
 //login page
 router.get("/", decorateHtmlResponse("Login"), getLogin);
+
+//process login
+router.post('/', login);
 
 module.exports = router;
